276f324dcf08a704c327ac2bb76c14285f5fc04d
Mekyi <no-reply>
2017-11-26 14:20:00 +0200

    commented code

--- raspi_scripts/weather_data/weather_data.py
+++ raspi_scripts/weather_data/weather_data.py
@@ -1,9 +1,7 @@
 #! python3 - weather_data
 
 import requests
-import openpyxl
 import json
-import sys
 import datetime
 import time
 import xml.etree.ElementTree as ET
@@ -16,8 +14,8 @@ def get_time():  # Get time and format it for the link
 # Constants
 API_KEY = 'a42452aa-ef80-4041-9668-4aa5007f61ef'
 PLACE = 'lahti'
-START_TIME = get_time()
-END_TIME = get_time()
+START_TIME = get_time()  # Weather data from (time)
+END_TIME = get_time()  # Weather data to (time)
 TIMESTEP = 60  # min
 SLEEPTIME = 900  # s
 LINK = ('http://data.fmi.fi/fmi-apikey/%s/wfs?request=getFeature'
@@ -25,33 +23,32 @@ LINK = ('http://data.fmi.fi/fmi-apikey/%s/wfs?request=getFeature'
     '&place=%s&parameters=Temperature,WeatherSymbol3&starttime=%s&endtime=%s'
     '&timestep=%s') % (API_KEY, PLACE, START_TIME, END_TIME, str(TIMESTEP))
 
-def get_data(url):
+def get_data(url):  # Request data from api server
     response = requests.get(url)
     response.raise_for_status()
     return (response.text)
 
-def xml_parse(data):
+def xml_parse(data):  # Parse text data into xml format
     root = ET.fromstring(data)
     return root
 
-def get_weather_data(XML_DATA):
+def get_weather_data(XML_DATA):  # Get weather and temperature parameters
     indexPosition = 0
     weatherData = {'temperature': 0, 'weather_icon': 0}
     for value in XML_DATA.iter('{http://www.opengis.net/waterml/2.0}value'):
         if indexPosition == 0:
-            weatherData['temperature'] = float(value.text)
+            weatherData['temperature'] = float(value.text)  # Temperature
         elif indexPosition == 1:
-            weatherData['weather_icon'] = float(value.text)
+            weatherData['weather_icon'] = float(value.text)  # Weather
         indexPosition += 1
     return weatherData
 
-def main():
+def main():  # Main loop
     while True:
         data = xml_parse(get_data(LINK))
-        print(get_weather_data(data))
-        with open('data.json', 'w') as fp:
+        with open('data.json', 'w') as fp:  # Store data into JSON file
             json.dump(get_weather_data(data), fp)
-        time.sleep(SLEEPTIME)
+        time.sleep(SLEEPTIME)  # Wait for next cycle
 
 if __name__ == "__main__":
     main()
