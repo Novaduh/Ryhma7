e7ca4722deb45f05250f716a88858d10eef451e0
Novaduh <32390080+Novaduh@users.noreply.github.com>
2017-11-26 15:00:20 +0200

    Update nappi.js

--- raspi_scripts/node_red/nappi.js
+++ raspi_scripts/node_red/nappi.js
@@ -1,7 +1,8 @@
-var count = context.get('Kys')||0;
-count += 1;
-context.set('Kys',count);
-msg.count = count +"count tallentaa";
-msg.payload = count;
-
+/* Tämä koodi laskee painikkeiden painallusten määrän. Koodi hakee liitetystä teksti tiedostosta arvon ja lisää siihen yhden lisään, 
+joka painalluksella.*/
+var count = context.get('Arvo')||0; //Haetaan arvo teksti tiedostosta
+count += 1;                         //Lisätään saatuun arvoon yksi lisää
+context.set('Arvo',count);          //Tallennetaan uusi arvo takaisin tiedostoon
+msg.count = count;                  //Asetetaan uusi arvo Node-rediin tarkasteltavaksi
+msg.payload = count;                
 return msg;
